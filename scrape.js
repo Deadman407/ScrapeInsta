5777b7e3886373e64212b64df84e9067d067c9d8
Deadman407 <dead.407.man@gmail.com>
2022-01-12 23:45:54 -0800

    Updated package.json

--- scrape.js
+++ scrape.js
@@ -1,16 +1,1 @@
-request = require('request')
-cheerio = require('cheerio')
 
-request('https://www.instagram.com/_vicky.knk._.04_/followers/', (error, response, html) => {
-    if(!error && response.statusCode == 200) {
-        const $ = cheerio.load(html)
-
-        const listOfFollowers = $('.wo9IH uu6c_ t2ksc enpQJ wFPL8')
-
-        listOfFollowers.each((i, el) => {
-            const follower = $(el)
-
-            console.log(follower)
-        })
-    }
-})
